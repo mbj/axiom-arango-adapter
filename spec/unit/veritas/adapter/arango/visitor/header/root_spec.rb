6b297759701117fe4b843714acb4fa3e32be7166
Markus Schirp <mbj@seonic.net>
2013-02-11 23:17:11 +0100

    Add unit specs for header visitor

---
+++ spec/unit/veritas/adapter/arango/visitor/header/root_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Veritas::Adapter::Arango::Visitor::Header, '#root' do
+  subject { object.root }
+
+  let(:node) { base.header }
+  let(:context) { mock('Context', :local_name => AQL.name_node(:local_name)) }
+
+  expect_aql <<-AQL
+    {"foo": `local_name`.`foo`, "bar": `local_name`.`bar`}
+  AQL
+end
