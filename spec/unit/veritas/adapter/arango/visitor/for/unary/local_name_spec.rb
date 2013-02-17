466104d87fcc112387a0e59083f9f4802b26b8c0
Markus Schirp <mbj@seonic.net>
2013-02-17 15:42:32 +0100

    Kill mutation in unary FOR visitor

---
+++ spec/unit/veritas/adapter/arango/visitor/for/unary/local_name_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Veritas::Adapter::Arango::Visitor::For::Unary, '#local_name' do
+  subject { object.local_name }
+  let(:object) { class_under_test.new(relation, context) }
+  let(:relation) { mock('Relation') }
+  let(:context)  { mock('Context') }
+
+  let(:local_name) { mock('Local Name') }
+
+  let(:class_under_test) do
+    local_name = self.local_name
+    Class.new(described_class) do
+      const_set('LOCAL_NAME', local_name)
+    end
+  end
+
+  it_should_behave_like 'an idempotent method'
+
+  it { should be(local_name) }
+end
