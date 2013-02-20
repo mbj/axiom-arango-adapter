66f78727dadc236a7dd68fc50840a2a8fcfcc9a2
Markus Schirp <mbj@seonic.net>
2013-02-20 16:04:24 +0100

    Add Adapter#read backed by Adapter#reader

--- lib/veritas/adapter/arango/adapter.rb
+++ lib/veritas/adapter/arango/adapter.rb
@@ -6,9 +6,9 @@ module Veritas
       class Adapter
         include Adamantium::Flat, Composition.new(:database)
 
-        # Return reader for relation
+        # Return reader for base relation
         #
-        # @param [Relation] relation
+        # @param [Relation::Base] base_relation
         #   the relation to read from
         #
         # @return [Reader]
@@ -16,8 +16,38 @@ module Veritas
         #
         # @api private
         #
-        def reader(relation)
-          Reader.new(database, relation)
+        def reader(base_relation)
+          Reader.new(database, base_relation)
+        end
+
+        # Read tuples from relation 
+        #
+        # @param [Relation::Base] base_relation
+        #
+        # @return [self]
+        #   if block given
+        #
+        # @return [Enumerable<Tuple>]
+        #   otherwise
+        #
+        # @api private
+        #
+        def read(base_relation, &block)
+          return to_enum(__method__, base_relation) unless block_given?
+          reader(base_relation).each(&block)
+          self
+        end
+
+        # Return gateway for arango adapter
+        #
+        # @param [Relation::Base] base_relation
+        #
+        # @return [Gateway]
+        #
+        # @api private
+        #
+        def gateway(base_relation)
+          Gateway.new(self, base_relation)
         end
 
       end
