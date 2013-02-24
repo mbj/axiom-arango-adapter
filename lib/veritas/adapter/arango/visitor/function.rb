c42f833e677ad4db7ca7ad76506d1cfa9642534a
Markus Schirp <mbj@seonic.net>
2013-02-24 22:58:45 +0100

    Add missing documentation for Static visitors

--- lib/veritas/adapter/arango/visitor/function.rb
+++ lib/veritas/adapter/arango/visitor/function.rb
@@ -2,6 +2,7 @@ module Veritas
   module Adapter
     module Arango
       class Visitor
+        # Base class for visitors with static root 
         class Static < self
 
           # Return root node
@@ -14,11 +15,13 @@ module Veritas
             self.class::ROOT
           end
 
+          # Visitor for Veritas::Function::Proposition::Tautology
           class Tautology < self
             ROOT = AQL::Node::Literal::Singleton::TRUE
             handle(Veritas::Function::Proposition::Tautology)
           end
 
+          # Visitor for Veritas::Function::Proposition::Contradiction
           class Contradiction < self
             ROOT = AQL::Node::Literal::Singleton::FALSE
             handle(Veritas::Function::Proposition::Contradiction)
